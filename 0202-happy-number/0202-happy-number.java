2fda4738c348ce89b5acc223887cd5c081832a8b
SRUAJN R <codewithsrujan.r@gmail.com>
2026-07-18 09:29:19 +0530

    Time: 1 ms (78.08%), Space: 42.2 MB (89.57%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,38 +1,28 @@
 class Solution {
     public boolean isHappy(int n) {
-             int slow=n;
-             int fast=n;
-           
-             while(fast!=1){
-               slow=sum_of_digits_square(slow);
-               if(slow==1){
-                return true;
-               }
-                fast=sum_of_digits_square(fast);
-                 fast=sum_of_digits_square(fast);
-                if(fast==slow){
-                    return false;
-                }
 
-            
-             }
-return true;
-             
-      
+       int slow=n;
+       int fast=sum_of_each_digits_square(n);
+       while(fast !=1 && slow!=fast){
+        slow=sum_of_each_digits_square(slow);
+        fast=sum_of_each_digits_square(fast);
+        fast=sum_of_each_digits_square(fast);
+
+        
+       }
+       return fast==1;
+
+
         
     }
-
-    public int sum_of_digits_square(int n){
+    public int sum_of_each_digits_square(int n){
         int d;
-        int square_sum=0;
+        int sum=0;
         while(n>0){
             d=n%10;
             n=n/10;
-            square_sum+=(d*d);
+            sum=sum+(d*d);
         }
-        return square_sum;
-
+        return sum;
     }
-   
-    
 }
