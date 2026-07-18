567a92bec514e16217023b854d11f9652e0a7ff8
SRUAJN R <codewithsrujan.r@gmail.com>
2026-07-18 10:39:49 +0530

    Time: 2 ms (87.05%), Space: 49.3 MB (35.85%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -1,44 +1,47 @@
 class Solution {
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null) {
-            return;
-        }
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
 
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
         }
-
-        ListNode cur = slow.next;
-        slow.next = null;
-        ListNode prev = null;
-        while (cur != null) {
-            ListNode next = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = next;
-        }
-
-        ListNode first = head;
-        ListNode second = prev;
+        ListNode sh=slow.next;
+          slow.next=null;
+       ListNode rev=reverse(sh);
         
-        ListNode dummy = new ListNode(0);
-        ListNode result = dummy;
+      
 
-        while (second != null) {
-            result.next = first;
-            first = first.next;
-            result = result.next;
+        ListNode first=head;
+        ListNode second=rev;
+           
+        while(first!=null && second !=null){
+               ListNode fn=first.next;
+               ListNode sn=second.next;
+              
+              first.next = second;
+    second.next = fn;
+               second=sn;
+               first=fn;
 
-            result.next = second;
-            second = second.next;
-            result = result.next;
+
         }
         
-        if (first != null) {
-            result.next = first;
+
+        
+         
+
+
+    }
+    public ListNode reverse(ListNode cur){
+        ListNode prev=null;
+        while(cur!=null){
+            ListNode next=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=next;
         }
+        return prev;
     }
 }
