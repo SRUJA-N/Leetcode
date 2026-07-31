a6b2b34f31d6196d94b663168f56a25af515c0c5
SRUAJN R <codewithsrujan.r@gmail.com>
2026-07-31 19:22:02 +0530

    Time: 1 ms (98.16%), Space: 47.3 MB (19.78%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -4,13 +4,30 @@ class Solution {
          if (intervals.length == 0) {
             return new int[][]{newInterval};
         }
+        int low=0;
+        int high=intervals.length-1;
+        int inx=intervals.length;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(intervals[mid][0]>newInterval[0]){
+               inx=mid;
+               high=mid-1;
+            }else{
+                low=mid+1;
+            }
 
-        int[][] combined=new int[intervals.length+1][2];
-        System.arraycopy(intervals,0,combined,0,intervals.length);
-
-        combined[intervals.length][0]=newInterval[0];
-        combined[intervals.length][1]=newInterval[1];
-        Arrays.sort(combined,(a,b)-> Integer.compare(a[0],b[0]));
+        }
+ int[][] combined=new int[intervals.length+1][2];
+        for(int i=0;i<inx;i++){
+             combined[i][0]=intervals[i][0];
+              combined[i][1]=intervals[i][1];
+        }
+        combined[inx][0]=newInterval[0];
+        combined[inx][1]=newInterval[1];
+        for(int i=inx;i<intervals.length;i++){
+            combined[i+1][0]=intervals[i][0];
+            combined[i+1][1]=intervals[i][1];
+        }
         int start1=combined[0][0];
         int end1=combined[0][1];
         int start2;
