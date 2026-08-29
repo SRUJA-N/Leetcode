010e77a877718664275d2f2e5b897a97223f2d1f
SRUAJN R <codewithsrujan.r@gmail.com>
2026-08-29 12:05:13 +0530

    Time: 11 ms (96.32%), Space: 47.8 MB (20.77%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,29 @@
 class Solution {
-    public class Pair{
-        int num;
-        int freq;
-        Pair(int n,int f){
-            this.num=n;
-            this.freq=f;
-        }
+   class Pair{
+    int number;
+    int frequency;
+    Pair(int number,int frequency){
+        this.number=number;
+        this.frequency=frequency;
     }
+   }
     public int[] topKFrequent(int[] nums, int k) {
-        PriorityQueue<Pair> minheap=new PriorityQueue<>(
-            (a,b)->a.freq-b.freq
-            
-        );
-        HashMap<Integer,Integer>  map=new HashMap<>();
-        for(int n:nums){
-            map.merge(n,1,Integer::sum);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        PriorityQueue<Pair> minheap=new PriorityQueue<>((a,b)-> a.frequency-b.frequency );
+       
+        for(int num:nums){
+            map.merge(num,1,Integer::sum);
         }
-        
-        for(int key:map.keySet()){
+         for(int key:map.keySet()){
             minheap.add(new Pair(key,map.get(key)));
             if(minheap.size()>k){
                 minheap.poll();
             }
-        }
-        int[] res= new int[k];
-        for(int i=0;i<k;i++){
-            res[i]=minheap.poll().num;
-        }
-        return res;
-
+         }
+         int[] res=new int[k];
+         for(int i=0;i<k;i++){
+            res[i]=minheap.poll().number;
+         }
+         return res;
     }
 }
