43e476d4baf50dbacfd5c74dfe63655bf39d6b45
SRUAJN R <codewithsrujan.r@gmail.com>
2026-09-12 23:09:17 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,24 +1,53 @@
 class Solution {
-    HashMap<Integer,Integer> dp=new HashMap<>();
-    public int helper(int i,int n)
-    {
-        if(i==n){
-            return 1;
-        }
-        if(i>n){
-            return 0;
-        }
-        if(dp.containsKey(i)){
-            return dp.get(i);
-        }
-        int a=helper(i+1,n);
-        int b=helper(i+2,n);
-        
-        int ans=a+b;
-        dp.put(i,ans);
-        return ans;
-    }
+   
     public int climbStairs(int n) {
-        return helper(0,n);
+        long[] fib=new long[]{1,
+1,
+2,
+3,
+5,
+8,
+13,
+21,
+34,
+55,
+89,
+144,
+233,
+377,
+610,
+987,
+1597,
+2584,
+4181,
+6765,
+10946,
+17711,
+28657,
+46368,
+75025,
+121393,
+196418,
+317811,
+514229,
+832040,
+1346269,
+2178309,
+3524578,
+5702887,
+9227465,
+14930352,
+24157817,
+39088169,
+63245986,
+102334155,
+165580141,
+267914296,
+433494437,
+701408733,
+1134903170,
+1836311903,0};
+
+return (int)fib[n];
     }
 }
