14f35275126fa7f0a1116df7a9ee3f4e9c171c0a
SRUAJN R <codewithsrujan.r@gmail.com>
2026-09-12 22:45:26 +0530

    Time: 3 ms (87.54%), Space: 52.7 MB (13.59%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,40 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int island = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    island++;
-                    bfsGrid(grid, i, j);
-                }
-            }
+        if(grid==null || grid.length==0||grid[0].length==0){
+            return 0;
         }
-        return island;
+     int count=0;
+     for(int i=0;i<grid.length;i++){
+        for(int j=0;j<grid[0].length;j++){
+            if(grid[i][j]=='1'){
+            dfs(grid,i,j);
+            count=count+1;
+        }
+        }
+     }
+        return count;
     }
-    
-    public void bfsGrid(char[][] grid, int R, int C) {
-        int row = grid.length;
-        int cols = grid[0].length;
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.offer(new int[]{R, C});
-        grid[R][C] = '0';
-
-        while (!q.isEmpty()) {
-            int[] num = q.poll();
-            int r = num[0]; 
-            int c = num[1]; 
-            
-            for (int[] dir : directions) {
-                int newR = dir[0] + r;
-                int newC = dir[1] + c;
-                
-                if (newR >= 0 && newC >= 0 && newR < row && newC < cols && grid[newR][newC] == '1') {
-                    grid[newR][newC] = '0';
-                    q.offer(new int[]{newR, newC});
-                }
-            }
+    void dfs(char[][] grid,int i,int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='0'){
+            return ;
         }
+        grid[i][j]='0';
+    
+        
+        dfs(grid,i+1,j);
+           dfs(grid,i-1,j);
+               dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+     
     }
 }
