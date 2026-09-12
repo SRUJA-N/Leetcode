b3c6f2a73a663eccce3997ce0b382245ed596f0d
SRUAJN R <codewithsrujan.r@gmail.com>
2026-09-12 23:37:35 +0530

    Time: 0 ms (100%), Space: 42.9 MB (19.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
-        int rob = 0;
-        int norob = 0;
-        for (int i = 0; i < nums.length; i ++) {
-            int newRob = norob + nums[i];
-            int newNoRob = Math.max(norob, rob);
-            rob = newRob;
-            norob = newNoRob;
+        int rob=0;
+        int norob=0;
+        for(int i=0;i<nums.length;i++){
+            int newrob=norob+nums[i];
+            int newnorob=Math.max(norob,rob);
+            rob=newrob;
+            norob=newnorob;
         }
-        return Math.max(rob, norob);
+        return Math.max(rob,norob);
     }
-    
 }
