99fad9fb5a2fd35fbe70395801b5af98890452b1
SRUAJN R <codewithsrujan.r@gmail.com>
2026-09-12 20:12:15 +0530

    Time: 0 ms (100%), Space: 46.9 MB (16.39%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,17 +11,19 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-               
+               if(head==null){
+                return false;
+               }
                ListNode slow=head;
-               ListNode fast=head;
+               ListNode fast=head.next;
                while(fast!=null && fast.next!=null){
-                slow=slow.next;
-                fast=fast.next.next;
-                if(slow==fast){
+                if(fast==slow){
                     return true;
                 }
-
+                slow=slow.next;
+                fast=fast.next.next;
                }
-      return false;
+               return false;
+              
     }
 }
