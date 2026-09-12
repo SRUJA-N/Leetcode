348c0998e378224da1826d4edcc94aaff6ff4db3
SRUAJN R <codewithsrujan.r@gmail.com>
2026-09-12 11:55:59 +0530

    Time: 8 ms (90.11%), Space: 48.8 MB (91.87%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -10,7 +10,7 @@ class Solution {
         List<int[]> res = new ArrayList<>();
         int end2;
         int start2;
-        for(int i=0;i<intervals.length;i++)
+        for(int i=1;i<intervals.length;i++)
         {
             start2=intervals[i][0];
             end2=intervals[i][1];
@@ -24,6 +24,6 @@ class Solution {
             end1=end2;
         }
        res.add(new int[]{start1,end1});
-        return res.toArray(new int[res.size()][]);
+        return res.toArray(new int[res.size()][2]);
     }
 }
