43b51d68481d739272922af20caefd3519bd9033
SRUAJN R <codewithsrujan.r@gmail.com>
2026-09-18 18:38:56 +0530

    Time: 3 ms (2.18%), Space: 46.3 MB (9.84%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,14 +1,23 @@
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int first=cost[0];
-        int second=cost[1];
-        for(int i=2;i<cost.length;i++)
-        {
-            int current=cost[i]+Math.min(first,second);
-            first=second;
-            second=current;
-
+    public int helper(int[] cost,int i,int n,HashMap<Integer,Integer> dp){
+        if(i>=n){
+            return 0; 
         }
-        return Math.min(first,second);
+        if(dp.containsKey(i)){
+            return dp.get(i);
+        }
+        int a=helper(cost,i+1,n,dp);
+        int b=helper(cost,i+2,n,dp);
+        int min=cost[i]+Math.min(a,b);
+        dp.put(i,min);
+
+        return dp.get(i);
+        
+    }
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+     HashMap<Integer,Integer> dp=new HashMap<Integer,Integer>();
+        
+       return Math.min(helper(cost, 0, n, dp), helper(cost, 1, n, dp));
     }
 }
