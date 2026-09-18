a2de783e32c1623b7b126bbef3d9a6fd4efc0f53
SRUAJN R <codewithsrujan.r@gmail.com>
2026-09-18 18:11:16 +0530

    Time: 1 ms (1.16%), Space: 42.4 MB (17.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,22 @@
 class Solution {
-   
-    public int climbStairs(int n) {
-     if(n <= 2) return n;
+    public int helper(int n,HashMap<Integer,Integer> dp,int i){
+        if(i>n){
+            return 0;
 
-        int prev1=1;
-        int prev2=2;
-        for(int i=3;i<=n;i++){
-            int current=prev1+prev2;
-            prev1=prev2;
-            prev2=current;
         }
-        return prev2;
+        if(i==n){
+            return 1;
+        }
+        if(dp.containsKey(i)){
+            return dp.get(i);
+        }
+        int a=helper(n,dp,i+1);
+        int b=helper(n,dp,i+2);
+        dp.put(i,a+b);
+
+        return dp.get(i);
+    }
+    public int climbStairs(int n) {
+       return helper(n,new HashMap<Integer,Integer>(),0);
     }
 }
